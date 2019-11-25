872da33ad403ce75b64164d40f3575d133947d1a
Eugene Xu <eugene_xu@epam.com>
2019-11-25 13:00:46 +0800

    rewrite task1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import { stdin, stdout } from 'process';
 
+stdin.setEncoding('utf8');
+
+stdin.on('readable', () => {
+  // read string from input
+  const chunk = stdin.read();
+  // reverse the string
+  const reverse = chunk.trim().split('').reverse().join('');
+  // print the reversed string
+  stdout.write(`${reverse}\n\n\n`);
+});
+
+stdin.on('end', () => {
+  stdout.write('end');
+});
