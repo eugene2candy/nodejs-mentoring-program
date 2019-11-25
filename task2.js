69a9d15737242aa66538d8fde1a2b7ed5addc626
Eugene Xu <eugene_xu@epam.com>
2019-11-25 12:40:45 +0800

    task2 finished

--- task2.js
+++ task2.js
@@ -1,6 +1,16 @@
 const csvPath = './csv/node_mentoring_t1_2_input_example.csv';
 const csv = require('csvtojson');
+const fs = require('fs');
 
 csv().fromFile(csvPath).then((jsonObj) => {
-  console.log(jsonObj);
+//   console.log(jsonObj);
+//   console.log(typeof jsonObj);
+  if (jsonObj) {
+    jsonObj.forEach((value) => {
+      fs.appendFile('task2.txt', `${JSON.stringify(value)}\n`, (err) => {
+        if (err) throw err;
+        console.log('write json to a txt... Done!');
+      });
+    });
+  }
 });
