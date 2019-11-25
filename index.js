159ff8f720157d793803bc7b2f57f533dbff58eb
Eugene Xu <eugene_xu@epam.com>
2019-11-25 13:08:18 +0800

    finished task3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { stdin, stdout } from 'process';
+import csv from 'csvtojson';
+import fs from 'fs';
 
+// task1
 stdin.setEncoding('utf8');
-
 stdin.on('readable', () => {
   // read string from input
   const chunk = stdin.read();
@@ -10,7 +12,18 @@ stdin.on('readable', () => {
   // print the reversed string
   stdout.write(`${reverse}\n\n\n`);
 });
-
 stdin.on('end', () => {
   stdout.write('end');
+});
+
+// task2
+const csvPath = './csv/node_mentoring_t1_2_input_example.csv';
+csv().fromFile(csvPath).then((jsonObj) => {
+  if (jsonObj) {
+    jsonObj.forEach((value) => {
+      fs.appendFile('re_task2.txt', `${JSON.stringify(value)}\n`, (err) => {
+        if (err) throw err;
+      });
+    });
+  }
 });
