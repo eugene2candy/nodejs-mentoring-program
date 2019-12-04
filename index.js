5bb7c0ff9233ea58c41c422801aea8c9648fb500
Chengjie Xu <chengjiexu@Chengjies-MBP.princeton.epam.com>
2019-12-04 14:17:54 +0800

    modify task3 with task2  solution

--- index.js
+++ index.js
@@ -18,12 +18,37 @@ stdin.on('end', () => {
 
 // task2
 const csvPath = './csv/node_mentoring_t1_2_input_example.csv';
-csv().fromFile(csvPath).then((jsonObj) => {
-  if (jsonObj) {
-    jsonObj.forEach((value) => {
-      fs.appendFile('re_task2.txt', `${JSON.stringify(value)}\n`, (err) => {
-        if (err) throw err;
+// csv().fromFile(csvPath).then((jsonObj) => {
+//   if (jsonObj) {
+//     jsonObj.forEach((value) => {
+//       fs.appendFile('re_task2.txt', `${JSON.stringify(value)}\n`, (err) => {
+//         if (err) throw err;
+//       });
+//     });
+//   }
+// });
+const writable = fs.createWriteStream('./re_task2.txt', 'utf8');
+fs.createReadStream(csvPath)
+  .on('data', (data) => {
+    csv({
+      noheader: false,
+      headers: ['book', 'author', 'amount', 'price'],
+      colParser: {
+        column1: 'string',
+        column2: 'string',
+        column3: 'number',
+        column4: 'number',
+      },
+      checkType: true,
+    }).fromString(data.toString()).then((jsonObj) => {
+      jsonObj.forEach((p) => {
+        writable.write(`${JSON.stringify(p)}\n`);
       });
     });
-  }
-});
+  })
+  .on('close', () => {
+    console.log('file was close');
+  })
+  .on('error', (e) => {
+    console.error(e);
+  });
