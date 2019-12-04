138ddb03a6860471542e7b19eeba0f27500465f1
Eugene Xu <eugene_xu@epam.com>
2019-12-04 16:48:08 +0800

    improve task2 & add some related links as comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { stdin, stdout } from 'process';
 import csv from 'csvtojson';
 import fs from 'fs';
+import { pipeline } from 'stream';
 
 // task1
 stdin.setEncoding('utf8');
@@ -18,37 +19,26 @@ stdin.on('end', () => {
 
 // task2
 const csvPath = './csv/node_mentoring_t1_2_input_example.csv';
-// csv().fromFile(csvPath).then((jsonObj) => {
-//   if (jsonObj) {
-//     jsonObj.forEach((value) => {
-//       fs.appendFile('re_task2.txt', `${JSON.stringify(value)}\n`, (err) => {
-//         if (err) throw err;
-//       });
-//     });
-//   }
-// });
 const writable = fs.createWriteStream('./re_task2.txt', 'utf8');
-fs.createReadStream(csvPath)
-  .on('data', (data) => {
-    csv({
-      noheader: false,
-      headers: ['book', 'author', 'amount', 'price'],
-      colParser: {
-        column1: 'string',
-        column2: 'string',
-        column3: 'number',
-        column4: 'number',
-      },
-      checkType: true,
-    }).fromString(data.toString()).then((jsonObj) => {
-      jsonObj.forEach((p) => {
-        writable.write(`${JSON.stringify(p)}\n`);
-      });
-    });
-  })
-  .on('close', () => {
-    console.log('file was close');
-  })
-  .on('error', (e) => {
-    console.error(e);
-  });
+const readable = fs.createReadStream(csvPath);
+const csvStream = csv({
+  noheader: false,
+  headers: ['book', 'author', 'amount', 'price'],
+  colParser: {
+    column1: 'string',
+    column2: 'string',
+    column3: 'number',
+    column4: 'number',
+  },
+  checkType: true,
+});
+pipeline(
+  readable,
+  csvStream,
+  writable,
+  (e) => {
+    if (e) {
+      console.error(e);
+    }
+  },
+);
--- task2.js
+++ task2.js
@@ -1,60 +1,36 @@
 const csvPath = './csv/node_mentoring_t1_2_input_example.csv';
 const csv = require('csvtojson');
 const fs = require('fs');
-const es = require('event-stream');
+const { pipeline } = require('stream');
 
-// csv().fromFile(csvPath).then((jsonObj) => {
-// //   console.log(jsonObj);
-// //   console.log(typeof jsonObj);
-//   if (jsonObj) {
-//     jsonObj.forEach((value) => {
-//       fs.appendFile('task2.txt', `${JSON.stringify(value)}\n`, (err) => {
-//         if (err) throw err;
-//         console.log('write json to a txt... Done!');
-//       });
-//     });
-//   }
-// });
+// improvement
 
-// remake
+// https://nodejs.org/api/fs.html#fs_fs_createreadstream_path_options
 const writable = fs.createWriteStream('./task2.txt', 'utf8');
-fs.createReadStream(csvPath)
-  .on('data', (data) => {
-    csv({
-      noheader: false,
-      headers: ['book', 'author', 'amount', 'price'],
-      colParser: {
-        column1: 'string',
-        column2: 'string',
-        column3: 'number',
-        column4: 'number',
-      },
-      checkType: true,
-    }).fromString(data.toString()).then((jsonObj) => {
-      jsonObj.forEach((p) => {
-        writable.write(`${JSON.stringify(p)}\n`);
-      });
-    });
-  })
-  .on('close', () => {
-    console.log('file was close');
-  })
-  .on('error', (e) => {
-    console.error(e);
-  });
+// https://nodejs.org/api/fs.html#fs_fs_createwritestream_path_options
+const readable = fs.createReadStream(csvPath);
+// https://github.com/Keyang/node-csvtojson#column-parser
+// https://github.com/Keyang/node-csvtojson#header-row
+const csvStream = csv({
+  noheader: false,
+  headers: ['book', 'author', 'amount', 'price'],
+  colParser: {
+    column1: 'string',
+    column2: 'string',
+    column3: 'number',
+    column4: 'number',
+  },
+  checkType: true,
+});
 
-// // remake again
-// const readable = fs.createReadStream(csvPath);
-// const writable = fs.createWriteStream('./task2.txt', 'utf8');
-
-// readable
-//   .pipe(es.split())
-//   .pipe(
-//     es.mapSync((line) => {
-//       console.log(line);
-//       writable.write(line);
-//     }),
-//   )
-//   .on('error', (e) => {
-//     console.log(e);
-//   });
+// https://nodejs.org/api/stream.html#stream_stream_pipeline_streams_callback
+pipeline(
+  readable,
+  csvStream,
+  writable,
+  (e) => {
+    if (e) {
+      console.error(e);
+    }
+  },
+);
