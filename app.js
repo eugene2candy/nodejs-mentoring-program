91d809f89240cc340e311cf1761ede9986bd2090
eugene2candy <105043487@qq.com>
2020-02-16 18:45:35 +0800

    first commit for HW5

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
+const rfs = require('rotating-file-stream');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(logger('dev'));
+
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: path.join(__dirname, 'logs')
+});
+
+let getMethods = 0;
+let postMethods = 0;
+let putMethods = 0;
+let deleteMethods = 0;
+
+logger.token('getMethod', req => {
+    let currentMethod;
+    if (req.method === 'GET') {
+        getMethods += 1;
+        currentMethod = getMethods;
+    } else if (req.method === 'POST') {
+        postMethods += 1;
+        currentMethod = postMethods;
+    } else if (req.method === 'PUT') {
+        putMethods += 1;
+        currentMethod = putMethods;
+    } else if (req.method === 'DELETE') {
+        deleteMethods += 1;
+        currentMethod = deleteMethods;
+    }
+    return `${req.method} Methods called: ${currentMethod} time(s)`;
+});
+
+logger.token('body', req => `Passing Form: ${JSON.stringify(req.body)}`);
+logger.token('err', res => res.res.statusMessage);
+
+const loggerFormat = 'Log Message: [:date[web]] ":method :url" [Status Code: :status] [Response Time: :response-time ms] [:body] :err';
+
+app.use(logger(loggerFormat));
+app.use(logger(':getMethod'));
+app.use(logger(loggerFormat, { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
 
-app.get(
-    '*',
-    (req, res) =>
-        // eslint-disable-next-line implicit-arrow-linebreak
-        res.status(200).send({
-            message: 'Welcome! '
-        })
-    // eslint-disable-next-line function-paren-newline
-);
+app.get('*', (req, res) => res.status(500).send({ message: 'Method Not Existed! ' }));
+app.post('*', (req, res) => res.status(500).send({ message: 'Method Not Existed! ' }));
+app.put('*', (req, res) => res.status(500).send({ message: 'Method Not Existed! ' }));
+app.delete('*', (req, res) => res.status(500).send({ message: 'Method Not Existed! ' }));
+
+process.on('uncaughtException', (err, origin) => {
+    app.use(logger(`Caught exception: ${err}\nException origin: ${origin}`));
+});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,7 @@ const usersController = require('../controllers').users;
 const groupsController = require('../controllers').groups;
 
 module.exports = app => {
-    app.get(
-        '/api',
-        (req, res) =>
-            // eslint-disable-next-line implicit-arrow-linebreak
-            res.status(200).send({
-                message: 'Welcome to the Users API!'
-            })
-        // eslint-disable-next-line function-paren-newline
-    );
+    app.get('/api', (req, res) => res.status(200).send({ message: 'Welcome to the Users API!' }));
 
     app.post('/user', usersController.create);
     app.get('/user', usersController.list);
